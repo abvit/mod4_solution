744e1ae1cd841c5302c28e5487230a225255d8ce
Abraham <k.abraham96@gmail.com>
2018-10-19 19:58:00 +0530

    Update SpeakGoodBye.js

---
+++ SpeakGoodBye.js
@@ -0,0 +1,18 @@
+
+( function(window){
+
+
+
+var speakWord = "Good Bye";
+
+
+var byeSpeaker ={}
+ byeSpeaker.speak = function(name){
+
+  console.log(speakWord + " " + name);
+}
+
+window.byeSpeaker = byeSpeaker;
+
+}
+	)(window);
